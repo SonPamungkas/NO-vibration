421075c052938b56929407e823f63de152183eab
SonPamungkas <nononon928@gmail.com>
2026-05-18 10:45:48 +0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace UKButt
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "dev.pitr.ukbutt";
+        public const string NAME = "UKButt";
+        public const string VERSION = "1.5.0";
+    }
+}
